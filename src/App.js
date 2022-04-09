637588feab9b830543376cd03864614372803f47
RoCristal <mail@rominacristaldo.com>
2022-04-09 13:58:46 +0200

    Added github link

--- src/App.js
+++ src/App.js
@@ -4,6 +4,17 @@ export default function App() {
   return (
     <div className="App">
       <header className="App-header">App</header>
+      <div>
+        Romina Cristaldo
+        <a
+          href="https://github.com/RoCristal/Horror-movies"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          Github
+        </a>
+      </div>
     </div>
   );
 }
